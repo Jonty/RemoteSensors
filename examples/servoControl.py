b26fba62675db0ca3ed2acf4bade4b335890f98d
Jonty <jonty@jonty.co.uk>
2009-12-28 23:49:20 +0000

    Add lame smoothing to servo movements by ignoring values sent while still in motion

--- examples/servoControl.py
+++ examples/servoControl.py
@@ -5,7 +5,11 @@ file = sock.makefile("rb")
 sock.connect(("192.168.0.250", 8000))
 sock.send("GET /?continuous=1&delay=0 HTTP/1.0\r\n\r\n")
 
-ser = serial.Serial('/dev/ttyUSB1', 19200, timeout=1)
+ser = serial.Serial('/dev/ttyUSB0', 19200, timeout=1)
+
+oldpitch = oldroll = 0;
+lastpitch = lastroll = 0;
+pitchc = rollc = 0
 
 while True:
     try:
@@ -17,18 +21,51 @@ while True:
     pitch = abs(int(result['pitch']) - 90)
     roll = abs(int(result['roll']) - 90)
 
+    if oldpitch == 0:
+        oldpitch = pitch
+    if oldroll == 0:
+        oldroll = roll
+
     if pitch > 170:
         pitch = 170
     if pitch < 10:
         pitch = 10
+
+    if pitch == lastpitch:
+        pitchc += 1
+    else:
+        pitchc = 0
+
+    setpitch = pitch
+    if abs(pitch - lastpitch) > 15 or pitchc < 3:
+        setpitch = oldpitch
+
+    if setpitch != oldpitch:
+        pitchc = 0
+        oldpitch = setpitch
+        ser.write("%dx" % setpitch)
+
+    lastpitch = pitch
 
     if roll > 170:
         roll = 170
     if roll < 10:
         roll = 10
 
-    serstring = "%dx%dy" % (pitch, roll)
-    print serstring
-    ser.write(serstring)
+    if roll == lastroll:
+        rollc += 1
+    else:
+        rollc = 0
+
+    setroll = roll
+    if abs(roll - lastroll) > 15 or rollc < 3:
+        setroll = oldroll
+
+    if setroll != oldroll:
+        rollc = 0
+        oldroll = setroll
+        ser.write("%dy" % setroll)
+
+    lastroll = roll
 
 ser.close()
